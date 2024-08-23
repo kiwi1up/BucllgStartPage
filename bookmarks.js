748cd108deab4ab33b7900a4e0d2b0087249315e
kiwi1up <greenstrike20@gmail.com>
2024-08-23 17:54:01 +0300

    Add files via upload

---
+++ bookmarks.js
@@ -0,0 +1,29 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "",
+    links: [
+      { name: "BucLLG Bans", url: "https://bucuresti-csgo.ro/panel/list/bans" },
+      { name: "Discord Invite", url: "https://discord.com/invite/uV7GFG7JHq" },
+      { name: "Forum", url: "https://laleagane.ro/forum/bucuresti/" },
+	  { name: "Steam Group", url: "https://steamcommunity.com/groups/BucLLgRo" },
+	  
+    ],
+  },
+  {
+    title: "",
+    links: [
+      { name: "Steamid.io", url: "https://steamid.io/" },
+      { name: "Online Players", url: "https://cs2browser.com/gameserver/2369/" },
+      { name: "Steam Search Players", url: "https://steamcommunity.com/search/users" },
+    ],
+  },
+  
+  {
+    title: "",
+    links: [
+      { name: "Kiwi commands", url: "https://steamcommunity.com/sharedfiles/filedetails/?id=3026444227" },
+	  { name: "Preturi Vip", url: "https://discord.com/channels/441993537572175872/1195663721540890804" },
+    ],
+  },
+];
